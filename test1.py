4f94c88bf8af0403439a4c3b4b57ada77f2730ff
Bapon <chowdhurybapon@gmail.com>
2013-05-09 14:12:46 +0600

    mysql sequre installation

--- test1.py
+++ test1.py
@@ -1,4 +1,13 @@
+import subprocess
 def main():
-    print "hello"
+    #print"hopo"
+    #subprocess.call("pwd")
+    #print "mysql"
+    #subprocess.call(["zypper", "install", "mysql-community-server"])
+    #subprocess.call("reboot")
+    #subprocess.call(["systemctl", "enable", "mysql.service"])
+    #subprocess.call(["systemctl","start", "mysql.service"])
+    subprocess.call("mysql_secure_installation")
 if __name__ == "__main__":
-   main()
+   main()  
+   
