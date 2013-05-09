8a2fa422bb56e4ec0a57fa42abade8fed4928aa8
Bapon <chowdhurybapon@gmail.com>
2013-05-09 14:25:24 +0600

    apache

--- test1.py
+++ test1.py
@@ -7,7 +7,10 @@ def main():
     #subprocess.call("reboot")
     #subprocess.call(["systemctl", "enable", "mysql.service"])
     #subprocess.call(["systemctl","start", "mysql.service"])
-    subprocess.call("mysql_secure_installation")
+    #subprocess.call("mysql_secure_installation")
+    subprocess.call(["zypper", "install", "apache2"])
+    subprocess.call(["systemctl", "enable", "apache2.service"])
+    subprocess.call(["systemctl", "start", "apache2.service"])
 if __name__ == "__main__":
    main()  
    
