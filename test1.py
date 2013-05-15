d6fd6b5c6e1abb3668920f85cf4c8b92d97a577e
Bapon <chowdhurybapon@gmail.com>
2013-05-15 18:48:03 +0600

    index.html

--- test1.py
+++ test1.py
@@ -1,4 +1,4 @@
-import sys, subprocess
+import sys, os, subprocess
 def main():
     #print"hopo"
     #subprocess.call("pwd")
@@ -17,8 +17,61 @@ def main():
     #code = ('''<?php
     # phpinfo();
     # ?>''')
-    # info_php.write(code) 
-    subprocess.call(["zypper", "install", "php5-mysql"])
+    #info_php.write(code) 
+    #subprocess.call(["zypper", "install", "php5-mysql"])
+    #phpmyadmin = open("/etc/apache2/conf.d/phpMyAdmin.conf", "w")
+    #wcoad = ('''Alias/phpMyAdmin /srv/www/htdocs/phpMyAdmin
+    #Alias/phpmyadmin /srv/www/htdocs/phpMyAdmin''')
+    #phpmyadmin.write(wcoad)
+    #subprocess.call(["systemctl", "restart", "apache2.service"])
+    #subprocess.call(["a2enmod", "rewrite"])
+    #print "virtual host"
+    #f_in=open("/etc/apache2/listen.conf").read()
+    #text=f_in.replace("#NameVirtualHost *:80", "NameVirtualHost *:80")
+    #f_out =open("/etc/apache2/listen.conf", "w").write(text)
+    #print"quantum.conf"
+    #bapon= open ("/etc/apache2/vhosts.d/vhost.template").read()
+    #text= bapon.replace("/vhosts/dummy-host.example.com/cgi-bin", "/cgi-bin")
+    #text=text.replace("dummy-host.example.com", "quantum.com")
+    #text=text.replace("Errorlog", "#Errorlog")
+    #text=text.replace('AllowOverride None', 'AllowOverride All')
+    #text=text.replace('ServerName quantum.com', ' ServerName quantum.com\n ServerAlias quantum.org.bd') 
+    #bapon1=open ("/etc/apache2/vhosts.d/quantum.conf", "w").write(text)  
+    print "_default"
+    
+    #file_in= open ("/etc/apache2/vhosts.d/quantum.conf").read()
+    #text=file_in.replace('vhosts/quantum.com', 'htdocs')
+    #text=text.replace('quantum.com', 'localhost')
+    #text=text.replace('ServerAlias quantum.org.bd', '#ServerAlias quantum.org.bd')
+    #file_out= open ("/etc/apache2/vhosts.d/_default.conf", 'w').write(text)
+    print "create index file"
+    #subprocess.call(["mkdir", "/srv/www/vhosts/quantum.com", "-p", "755"])
+    #gias=open("/srv/www/vhosts/quantum.com/index.html", "w")
+    #index_coad= "<html>it works</html>"
+    #gias.write(index_coad)
+    subprocess.call(["rcapache2","reload"])
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 if __name__ == "__main__":
    main()  
